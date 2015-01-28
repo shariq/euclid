0c07d29deae3a1b32dd559faab244a9b26ae8895
Shariq Hashme <shariqhashme@gmail.com>
2015-01-28 17:56:17 -0500

    untested app.py

--- server/app.py
+++ server/app.py
@@ -1,13 +1,44 @@
-from bottle import route, hook, response, run
 import cPickle
 import datetime
+import threading
+import time
+import json
+from bottle import route, hook, response, run
 
-data = cPickle.load('../data/Spring_2015.bin')
-globals()['thingy'] = 'yoyoyo'
+database = cPickle.load('../data/Spring_2015.bin')
+
+globals()['information'] = ''
+
+def updateInformation():
+    weekday = datetime.datetime.now().weekday()
+    time = datetime.datetime.now().time()
+    new_information = []
+    for course in database.keys():
+        description = database[course]['description']
+        title = database[course]['title']
+        for meeting in meetings:
+            if meeting['weekday'] != weekday:
+                continue
+            if meeting['start'] < time < meeting['end']:
+                new_information.append((title, description, meeting))
+    globals()['information'] = json.dumps(new_information)
+
+def updateThread():
+    while True:
+        updateInformation()
+        time.sleep(60)
+
+updateInformation()
+looper = threading.Thread(updateThread)
+looper.daemon = True
+looper.start()
 
 @route('/')
 def index():
-    return globals()['thingy']
+    return 'yoyoyo'
+
+@route('/data')
+def data():
+    return globals()['information']
 
 run(host = '0.0.0.0', port = 80, server = 'tornado')
-
