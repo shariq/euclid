968d050f557913572e4d1a77c8b4dec7efb6321d
Shariq Hashme <shariqhashme@gmail.com>
2015-01-30 20:09:14 -0500

    w0000 working :3

--- server/app.py
+++ server/app.py
@@ -5,12 +5,11 @@ import datetime
 import threading
 import time
 import json
+import random
 import re
 from bottle import route, hook, response, run, static_file
 
 database = cPickle.load(open('../data/spring15.bin', 'rb'))
-
-globals()['information'] = ''
 
 
 def updateInformation():
@@ -26,11 +25,28 @@ def updateInformation():
             del new_meeting['end']
             new_meeting['course'] = ', '.join(new_meeting['course'])
             new_information.append(new_meeting)
-    globals()['information'] = json.dumps(new_information)
-    with open('index.html') as f:
-        # this is absolutely horrible and we should use nginx
-        globals()['index_contents'] = f.read()
+    if len(new_information) < 2:
+        empty_meeting = {}
+        empty_meeting['course'] = 'n/a'
+        new_information.append(empty_meeting)
+    globals()['information_string'] = json.dumps(new_information)
+    globals()['information_list'] = map(json.dumps, new_information)
 
+with open('index.html') as f:
+    # this is absolutely horrible and we should use nginx
+    globals()['index_html_content'] = f.read()
+
+with open('index.css') as f:
+    # this is absolutely horrible and we should use nginx
+    globals()['index_css_content'] = f.read()
+
+with open('index.js') as f:
+    # this is absolutely horrible and we should use nginx
+    globals()['index_js_content'] = f.read()
+
+with open('quojs.js') as f:
+    # this is absolutely horrible and we should use nginx
+    globals()['quo_js_content'] = f.read()
 
 def updateThread():
     while True:
@@ -45,10 +61,31 @@ looper.start()
 
 @route('/')
 def index():
-    return globals()['index_contents']
+    return static_file('index.html', root = '.')
 
-@route('/data')
-def data():
-    return globals()['information']
+@route('/index.css')
+def index_css():
+    return static_file('index.css', root = '.')
 
-run(host = '0.0.0.0', port = 80, server = 'tornado')
+@route('/index.js')
+def index_js():
+    return static_file('index.js', root = '.')
+
+@route('/quojs.js')
+def index_js():
+    return static_file('quojs.js', root = '.')
+
+@route('/favicon.ico')
+def index_js():
+    return static_file('favicon.ico', root = '.')
+
+@route('/all')
+def all_data():
+    return globals()['information_string']
+
+@route('/random')
+def select_data():
+    return random.choice(globals()['information_list'])
+
+
+run(host = '0.0.0.0', port = 8080, server = 'tornado')
--- server/index.js
+++ server/index.js
@@ -5,7 +5,11 @@ $(document).ready(function() {
 
     function hmmDotting() {
         if (loaded[0] > 0) {
-            $('#suggestion')[0].textContent = 'hmm' + ['', '.', '..', '...'][4 - loaded[0]];
+            $('#course')[0].textContent = 'JAHA';
+            $('#title')[0].textContent = 'o' + ['.', '..', '...', '....'][4 - loaded[0]] + 'O';
+            $('#timing')[0].textContent = ['-', '--', '---', '---'][4 - loaded[0]];
+            $('#about')[0].textContent = ['__', '___', '____', '_____'][4 - loaded[0]];
+//            $('#description')[0].textContent = 'Hmm' + ['', '.', '..', '...'][4 - loaded[0]];
             loaded[0] = loaded[0] - 1;
         } else {
             clearInterval(intervalVar);
@@ -16,10 +20,41 @@ $(document).ready(function() {
         hmmDotting();
     }, 300);
 
-    function changeWord() {
-        $.get('http://burgundy.io:8080/', function(data) {
+    last_course = ['HUMAN'];
+
+    function changeClass() {
+        $.get('/random', function(data) {
             loaded[0] = 0;
-            $('#suggestion')[0].textContent = data;
+            json_data = JSON.parse(data);
+            if (json_data['course'] == 'n/a') {
+                $('#course')[0].textContent = 'XD';
+                $('#title')[0].textContent = ':-(';
+                $('#timing')[0].textContent = 'nothing';
+                $('#about')[0].textContent = 'left';
+//                $('#description')[0].textContent = '';
+                last_course[0] = json_data['course'];
+                return;
+            }
+            if (last_course[0] == json_data['course']) {
+                changeClass();
+                return;
+            }
+            last_course[0] = json_data['course'];
+            course = json_data['course'];
+            title = json_data['title'];
+            timing = json_data['start_s'] + ' - ';
+            timing += json_data['end_s'] + ' in ';
+            timing += json_data['location'];
+            about = json_data['type'] + ' with ';
+            about += json_data['size'] + ' seat';
+            if (json_data['size'] > 0) {
+                about += 's';
+            }
+            $('#title')[0].textContent = title;
+            $('#course')[0].textContent = course;
+            $('#timing')[0].textContent = timing;
+            $('#about')[0].textContent = about;
+//            $('#description')[0].textContent = json_data['description'];
             clearInterval(intervalVar);
         });
     }
@@ -27,13 +62,17 @@ $(document).ready(function() {
     function adjustFont() {
         windowWidth = parseInt($('html').css('width').replace('px',''));
         if (windowWidth > 850) windowWidth = 850;
-        $('#suggestion').css('font-size', windowWidth/7.5 + "px")
+        $('#course').css('font-size', windowWidth/7.5 + "px");
+        $('#title').css('font-size', windowWidth/22.5 + "px");
+        $('#timing').css('font-size', windowWidth/22.5 + "px");
+        $('#about').css('font-size', windowWidth/22.5 + "px");
+//        $('#description').css('font-size', windowWidth/30 + "px");
     }
 
     adjustFont();
-    changeWord();
+    changeClass();
 
     $(window).resize(adjustFont);
-    $(document).keypress(changeWord);
-    $$('body').tap(changeWord);
+    $(document).keypress(changeClass);
+    $$('body').tap(changeClass);
 });
